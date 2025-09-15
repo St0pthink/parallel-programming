e51b3b7f75d0079f9bff2e77309b0454f33aff1a
St0pthink <foljustfol@gmail.com>
2025-09-15 05:44:59 +0500

    lb1

---
+++ lb1.1/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace project
+{
+    class Program1
+    {
+        static void Main()
+        {
+            int N = 10000;
+            int M = 200;
+
+            for (int nb = 1; nb < 4; nb++)                                                                                //для задания 1.1 необходимо в цикле nb начинать не с 2, а с 1                               
+            {
+                if (nb == 1)
+                {
+                    N = 1000; Console.WriteLine("Эксперимент со значением N = 10000");                           //для задания 1.1
+                    if (nb == 2) { N = 1020; Console.WriteLine("Эксперимент со значением N = 1020, сопоставимо к кэшу L2"); }
+                    if (nb == 3) { N = 1250; Console.WriteLine("Эксперимент со значением N = 1250, сопоставимо к кэшу L3"); }
+                    for (int number = 1; number < 5; number++)
+                    {
+                        double time_f = 0, time_sum = 0, time_min = 99999, time_max = 0, time_mean = 0;
+                        double[,] a = new double[N, N];
+                        DateTime t1, t2;
+                        TimeSpan dt;
+
+                        if (number == 1)
+                        {
+                            for (int y = 0; y < M; y++)
+                            {
+                                a = new double[N, N];
+                                t1 = DateTime.Now;
+                                for (int i = 0; i < N; i++)
+                                    for (int j = 0; j < N; j++)
+                                        a[i, j] = i / (j + 1);
+                                t2 = DateTime.Now;
+                                dt = t2 - t1;
+                                time_f = dt.TotalMilliseconds / 1000.0;
+                                time_sum += time_f;
+                                if (time_max < time_f) { time_max = time_f; }
+                                if (time_min > time_f) { time_min = time_f; }
+                            }
+                            time_mean = time_sum / M;
+
+                            Console.WriteLine("Для цикла номер " + number);
+                            Console.WriteLine("Среднее время: " + Math.Round(time_mean, 4));
+                            Console.WriteLine("Минимальное время: " + time_min);
+                            Console.WriteLine("Максимальное время: " + time_max + "\n");
+                        }
+
+
+                        if (number == 2)
+                        {
+                            for (int y = 0; y < M; y++)
+                            {
+                                a = new double[N, N];
+                                t1 = DateTime.Now;
+                                for (int j = 0; j < N; j++)
+                                    for (int i = 0; i < N; i++)
+                                        a[i, j] = i / (j + 1);
+                                t2 = DateTime.Now;
+                                dt = t2 - t1;
+                                time_f = dt.TotalMilliseconds / 1000.0;
+                                time_sum += time_f;
+                                if (time_max < time_f) { time_max = time_f; }
+                                if (time_min > time_f) { time_min = time_f; }
+                            }
+                            time_mean = time_sum / M;
+
+                            Console.WriteLine("Для цикла номер " + number);
+                            Console.WriteLine("Среднее время: " + Math.Round(time_mean, 4));
+                            Console.WriteLine("Минимальное время: " + time_min);
+                            Console.WriteLine("Максимальное время: " + time_max + "\n");
+                        }
+
+                        if (number == 3)
+                        {
+                            for (int y = 0; y < M; y++)
+                            {
+                                a = new double[N, N];
+                                t1 = DateTime.Now;
+                                for (int i = N - 1; i >= 0; i--)
+                                    for (int j = N - 1; j >= 0; j--)
+                                        a[i, j] = i / (j + 1);
+                                t2 = DateTime.Now;
+                                dt = t2 - t1;
+                                time_f = dt.TotalMilliseconds / 1000.0;
+                                time_sum += time_f;
+                                if (time_max < time_f) { time_max = time_f; }
+                                if (time_min > time_f) { time_min = time_f; }
+                            }
+                            time_mean = time_sum / M;
+
+                            Console.WriteLine("Для цикла номер " + number);
+                            Console.WriteLine("Среднее время: " + Math.Round(time_mean, 4));
+                            Console.WriteLine("Минимальное время: " + time_min);
+                            Console.WriteLine("Максимальное время: " + time_max + "\n");
+                        }
+
+                        if (number == 4)
+                        {
+
+                            for (int y = 0; y < M; y++)
+                            {
+                                a = new double[N, N];
+                                t1 = DateTime.Now;
+                                for (int j = N - 1; j >= 0; j--)
+                                    for (int i = N - 1; i >= 0; i--)
+                                        a[i, j] = i / (j + 1);
+                                t2 = DateTime.Now;
+                                dt = t2 - t1;
+                                time_f = dt.TotalMilliseconds / 1000.0;
+                                time_sum += time_f;
+                                if (time_max < time_f) { time_max = time_f; }
+                                if (time_min > time_f) { time_min = time_f; }
+                            }
+                            time_mean = time_sum / M;
+
+                            Console.WriteLine("Для цикла номер " + number);
+                            Console.WriteLine("Среднее время: " + Math.Round(time_mean, 4));
+                            Console.WriteLine("Минимальное время: " + time_min);
+                            Console.WriteLine("Максимальное время: " + time_max + "\n");
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ lb1.2/task2/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using Microsoft.VisualBasic;
+class task2
+{
+    static int[] ns = {1000, 5000, 10000, 10000};
+    static int[] thrs = {1, 2, 4, 8, 12, 16, 20};
+    static  DateTime time0;
+    static TimeSpan time;
+    static void Exponentiation_(object o)
+    {
+        object[] info = (object[])o;
+        double[] a_array = (double[])info[2];
+        double[] b_array = (double[])info[3];
+        int left = (int)info[0];
+        int right = (int)info[1];
+        for (int i = left; i < right; i++)
+        {   
+            double sum = 0;
+            for (int j = 0; j < i + 1; j++)
+                sum += Math.Pow(a_array[j], 1.789);
+            if (i >= left)
+            {
+                b_array[i] = sum;
+            }
+        }
+    }
+    static double[] Fill(int n)
+    {
+        double[] array = new double[n];
+        for (int i = 0; i < n; i++)
+        {
+            array[i] = i + 1;
+        }
+        return array;
+    }
+    static double[] parallels_type1(double[] array, int n, int thr)
+    {
+        double[] b_array = new double[n];
+        int step = n / thr;
+        Thread[] threads = new Thread[thr];
+        for (int i = 0; i < thr; i++)
+        {
+            int left = i * step;
+            int right;
+            if (i == thr - 1)
+            {
+                right = n;
+            }
+            else
+            {
+                right = (i + 1) * step;
+            }
+            threads[i] = new Thread(Exponentiation_);
+            threads[i].Start(new object[4] { left, right, array, b_array});
+            
+        }
+
+        for (int i = 0; i < thr; i++)
+        {
+            threads[i].Join();
+        }
+        return b_array;
+    }
+    static double[] parallels_type2(double[] array, int n, int thr)
+    {
+        double[] b_array = new double[n];
+        Thread[] threads = new Thread[thr];
+        int right = 0;
+        int left = 0;
+        for (int i = 0; i < thr; i++)
+        {
+            double step = Math.Pow(2, i + 1);
+            right = left+(int)Math.Floor(n / step);
+            if (thr == 1 || i == thr - 1)
+            {
+                right = n;
+            }
+            threads[i] = new Thread(Exponentiation_);
+            threads[i].Start(new object[4] { left, right, array, b_array });
+            left = right;
+            
+        }
+
+        for (int i = 0; i < thr; i++)
+        {
+            threads[i].Join();
+        }
+        return b_array;
+    }
+    static void Main()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                time0 = DateTime.Now;
+                ///parallels_type1(Fill(ns[i]), ns[i], thrs[j]);
+                parallels_type2(Fill(ns[i]), ns[i], thrs[j]);
+                time = DateTime.Now - time0;
+                Console.WriteLine(time);
+            }
+            Console.WriteLine("/////////////////////////");
+        }
+
+    }
+}
+
