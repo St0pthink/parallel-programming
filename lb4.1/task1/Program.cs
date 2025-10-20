0ff311f7f090c6f63fd3d92a2b14bc77277868fd
St0pthinkHonor <foljustfol@gmail.com>
2025-10-20 15:51:51 +0500

    lb 4.1 done

---
+++ lb4.1/task1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using Microsoft.VisualBasic;
+class task2
+{
+    private static volatile string? Buff;
+    static void Main()
+    { 
+        int n = 5;
+        string input = Console.ReadLine()!;
+        string[] text = input.Split(';', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        int len = text.Length;     
+        var show = new Semaphore[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            show[i] = new Semaphore(0,1);
+        }
+
+        var confirm = new Semaphore(0, n);
+        
+        var consumers = new List<Thread>();
+
+        Thread developer = new(Give) { Name = "Производитель" };
+        developer.Start();
+        for (int i = 0; i < n; i++)
+        {
+            var consumer = new Thread(Take) { Name = $"Потребитель {i + 1}" };
+            consumer.Start(i);
+            consumers.Add(consumer);
+        }
+
+
+        developer.Join();            
+        foreach (var t in consumers) 
+            t.Join();
+        Console.WriteLine("Готово");
+
+        void Give(){
+            for (int i  = 0; i < len; i++)
+            {
+                Buff = text[i];
+                for (int j = 0; j < n; j++)
+                {
+                    show[j].Release();
+                }
+                 Console.WriteLine($"{Thread.CurrentThread.Name} передал сообщение: {Buff}");
+                for (int j = 0; j < n; j++)
+                {
+                    confirm.WaitOne();
+                }
+            }
+        }
+        void Take(object? state)
+        {
+            int num = (int)state!;
+            for (int y = 0; y < len; y++)
+            {
+                show[num].WaitOne();
+                Console.WriteLine($"{Thread.CurrentThread.Name} получило сообщение: {Buff}");
+                confirm.Release();
+            }
+        }
+    }
+}
